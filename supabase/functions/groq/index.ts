8324e1c19b0ba2dbc8516a22b22b4380020a2957
Maliket <RoyalAnaconda@icloud.com>
2025-10-22 12:42:47 +0300

    Enhance chat functionality with embedding and RAG

--- supabase/functions/groq/index.ts
+++ supabase/functions/groq/index.ts
@@ -1,11 +1,52 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { GoogleGenAI } from "npm:@google/genai";
 const token = Deno.env.get("GROQ_API_KEY");
 const supabaseUrl = Deno.env.get("SUPABASE_URL");
 const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
 const supabase = createClient(supabaseUrl, supabaseKey);
+const defaultPrompt = "{query}\n\nContext information is below, surrounded by ---------------------\n\n---------------------\n{question_answer_context}\n---------------------\n\nGiven the context and provided history information and not prior knowledge,\nreply to the user comment. If the answer is not in the context, inform\nthe user that you can't answer the question.\n";
+const geminiEmbeding = async (content)=>{
+  const ai = new GoogleGenAI({});
+  const response = await ai.models.embedContent({
+    model: 'gemini-embedding-001',
+    contents: content
+  });
+  return response.embeddings?.[0]?.values ?? [];
+};
+const similaritySearch = async (userMessageEmbedding, chatId)=>{
+  const { data, error } = await supabase.rpc("match_vectors", {
+    chat_id: chatId,
+    query_embedding: userMessageEmbedding,
+    match_count: 5
+  });
+  if (error) throw new Error(`Similarity error: ${error.message}`);
+  return data;
+};
+const buildRagContext = (userQuery, embedding)=>{
+  const content = embedding.map((e)=>e.content).join('');
+  const pattern = defaultPrompt.replace('{query}', userQuery).replace('{question_answer_context}', content);
+  return {
+    role: "system",
+    content: pattern
+  };
+};
+const getChatSettings = async (chatId)=>{
+  const { data, error } = await supabase.from('chats').select('settings').eq('id', chatId).single();
+  if (error) throw error;
+  return data.settings;
+};
 serve(async (req)=>{
   const { chatId, prompt, model = "llama-3.1-8b-instant" } = await req.json();
+  const vectorizedPromps = await geminiEmbeding(prompt);
+  const chatSettings = await getChatSettings(chatId);
+  console.log('hat settings: ', chatSettings);
+  console.log('Vector: ', vectorizedPromps);
+  const selectedEmbededText = await similaritySearch(vectorizedPromps, chatId);
+  console.log('Selected embeddings: ', selectedEmbededText);
+  const isRagEnabled = chatSettings?.is_rag_enabled ?? false;
+  const ragContext = isRagEnabled ? buildRagContext(prompt, selectedEmbededText) : null;
+  console.log('RAG CONTEXT: ', ragContext);
   {
     const { error } = await supabase.from("chat_messages").insert({
       content: prompt,
@@ -15,7 +56,7 @@ serve(async (req)=>{
     });
     if (error) console.error("User insert error:", error);
   }
-  const history = await supabase.from("chat_messages").select().then((resp)=>{
+  const history = await supabase.from("chat_messages").select().eq("chat_id", chatId).then((resp)=>{
     const messages = resp.data;
     console.log('MESSAGE: ', messages);
     return messages.map((mess)=>{
@@ -35,11 +76,14 @@ serve(async (req)=>{
       model,
       stream: true,
       messages: [
-        ...history,
         {
           role: "system",
           content: "You are a helpful assistant."
         },
+        ...ragContext ? [
+          ragContext
+        ] : [],
+        ...history,
         {
           role: "user",
           content: prompt
